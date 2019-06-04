2560f71648975104cb65cd7cd4fc585e8372ff04
Rodrigo <rodarima@gmail.com>
2019-06-04 10:48:47 +0200

    Add discrete model

---
+++ doc/record.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+DIR="$(dirname $0)"
+PDF="$DIR/thesis.pdf"
+LOG="$DIR/working.log"
+
+WORDS=$(pdftotext $PDF - | wc -w)
+PERCENT=$(printf "$WORDS/200\n" | calc -p)
+
+
+printf "$(date +%s)\t$WORDS\t$PERCENT\n" >> "$LOG"
