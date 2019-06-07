ac2228b131f1836de4a331483f4f9f77b12232e2
Rodrigo <rodarima@gmail.com>
2019-06-07 19:07:16 +0200

    Begin sequential simulation design

--- doc/record.sh
+++ doc/record.sh
@@ -4,8 +4,13 @@ DIR="$(dirname $0)"
 PDF="$DIR/thesis.pdf"
 LOG="$DIR/working.log"
 
+
 WORDS=$(pdftotext $PDF - | wc -w)
 PERCENT=$(printf "$WORDS/200\n" | calc -p)
 
 
 printf "$(date +%s)\t$WORDS\t$PERCENT\n" >> "$LOG"
+
+cd "$DIR"
+
+python regression.py
--- doc/regression.py
+++ doc/regression.py
@@ -1,5 +1,9 @@
+#!/usr/bin/env python
+
 import numpy as np
 from sklearn.linear_model import LinearRegression
+import matplotlib as mpl
+mpl.use('Agg')
 import matplotlib.pyplot as plt
 
 
@@ -8,25 +12,30 @@ data = np.genfromtxt('working.log', delimiter='\t')
 
 t = data[:,0].reshape(-1, 1)
 percent = data[:,2].reshape(-1, 1)
-print(t)
-print(percent)
+#print(t)
+#print(percent)
 
 reg = LinearRegression().fit(t, percent)
 reg.score(t, percent)
 
-print(reg.coef_)
+#print(reg.coef_)
 
-print(reg.intercept_)
+#print(reg.intercept_)
 
 t0 = np.min(t)
 tf = 1560894600
+tm = 1560376200
 
 tt = np.linspace(t0, tf, 100).reshape(-1, 1)
 
 pf = reg.predict(tt)
 
-plt.plot(t, percent, 'bo')
-plt.plot(tt, pf, 'r')
+plt.plot(tt, pf, 'g')
+plt.scatter(t, percent, s=10, color='k')
+plt.axvline(x=tf, color='r', linewidth=3.0)
+plt.axvline(x=tm, color='k', linewidth=1.0)
 plt.xlim(np.min(t), tf)
 plt.ylim(0, 100)
-plt.show()
+plt.grid(True)
+#plt.show()
+plt.savefig("prediction.png")
