491108ee7061280907fe882ce935aa21ccb6b272
Rodrigo <rodarima@gmail.com>
2019-06-16 16:29:04 +0200

    Debug conf for extrae and ignore traces

---
+++ extrae/debug.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#export EXTRAE_HOME=/usr
+
+source ${EXTRAE_HOME}/etc/extrae.sh
+
+export EXTRAE_CONFIG_FILE=extrae/extrae2.xml
+#export LD_PRELOAD=${EXTRAE_HOME}/lib/libmpitrace.so # For C apps
+#export LD_PRELOAD=${EXTRAE_HOME}/lib/libnanosmpitrace.so
+export LD_PRELOAD=${EXTRAE_HOME}/lib/libnanostrace.so
+
+export EXTRAE_ON=1
+#export NANOS6_REPORT_PREFIX="@@@"
+#export NANOS6=debug
+
+# For extrae
+#export NANOS6=extrae
+
+#For heavy debug
+export NANOS6=verbose-debug
+export NANOS6_VERBOSE=all
+export NANOS6_EXTRAE_AS_THREADS=1
+
+## Run the desired program
+#NANOS6=extrae
+$*
