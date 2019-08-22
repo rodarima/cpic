37a939c3cfcf8706f8e0e6343dba521e955d4e87
Rodrigo <rodarima@gmail.com>
2019-08-22 12:38:46 +0200

    Plot the fields in X-Y plane, fixes #4

--- src/pv.c
+++ src/pv.c
@@ -101,42 +101,96 @@ pv_dump_particles(sim_t *sim)
 }
 
 int
+write_xdmf_fields(sim_t *sim)
+{
+	FILE *f;
+	char file[PATH_MAX];
+	char dataset[PATH_MAX];
+	mat_t *rho, *phi;
+
+	rho = sim->field.rho;
+	phi = sim->field.phi;
+
+	snprintf(file, PATH_MAX-1, "data/fields-iter%d.xdmf",
+			sim->iter);
+
+	snprintf(dataset, PATH_MAX-1, "fields-iter%d.h5",
+			sim->iter);
+
+	f = fopen(file, "w");
+
+	fprintf(f, "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n");
+	fprintf(f, "<Xdmf xmlns:xi=\"http://www.w3.org/2001/XInclude\" Version=\"3.0\">\n");
+	fprintf(f, "  <Domain>\n");
+	fprintf(f, "    <Grid Name=\"fields\">\n");
+	fprintf(f, "      <Topology TopologyType=\"3DCoRectMesh\" NumberOfElements=\"%d %d %d\"/>\n",
+			1, rho->shape[Y], rho->real_shape[X]);
+	fprintf(f, "      <Geometry Origin=\"\" Type=\"ORIGIN_DXDYDZ\">\n");
+	fprintf(f, "        <DataItem Format=\"XML\" Dimensions=\"3\">\n");
+	fprintf(f, "            0.0 0.0 0.0\n");
+	fprintf(f, "        </DataItem>\n");
+	fprintf(f, "        <DataItem Format=\"XML\" Dimensions=\"3\">\n");
+	fprintf(f, "            %f %f %f\n", sim->dx[Z], sim->dx[Y], sim->dx[X]);
+	fprintf(f, "        </DataItem>\n");
+	fprintf(f, "      </Geometry>\n");
+	fprintf(f, "      <Attribute Center=\"Node\" Name=\"PHI\" DataType=\"Scalar\">\n");
+	fprintf(f, "        <DataItem Dimensions=\"%d %d %d\" DataType=\"Float\" Precision=\"8\" Format=\"HDF\">%s:/phi</DataItem>\n",
+			1, phi->shape[Y], phi->real_shape[X], dataset);
+	fprintf(f, "      </Attribute>\n");
+	fprintf(f, "      <Attribute Center=\"Node\" Name=\"RHO\" DataType=\"Scalar\">\n");
+	fprintf(f, "        <DataItem Dimensions=\"%d %d %d\" DataType=\"Float\" Precision=\"8\" Format=\"HDF\">%s:/rho</DataItem>\n",
+			1, rho->shape[Y], rho->real_shape[X], dataset);
+	fprintf(f, "      </Attribute>\n");
+	fprintf(f, "    </Grid>\n");
+	fprintf(f, "  </Domain>\n");
+	fprintf(f, "</Xdmf>\n");
+
+	fclose(f);
+
+	return 0;
+}
+
+int
 pv_dump_fields(sim_t *sim)
 {
 	char file[PATH_MAX];
-	mat_t *rho, *phi;
+	mat_t *rho, *phi, *_phi;
 	int ix, iy;
 
 	hid_t file_id, dataset_id, dataspace_id;
 	herr_t status;
 	hsize_t dims[2];
 
+	write_xdmf_fields(sim);
+
 	snprintf(file, PATH_MAX-1, "data/fields-iter%d.h5",
 			sim->iter);
 
-	rho = sim->field._rho;
+	rho = sim->field.rho;
 	/* Create a new file using default properties. */
 	file_id = H5Fcreate(file, H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT);
 
-	//dims[X] = rho->shape[X];
-	//dims[Y] = rho->shape[Y];
+	/* We assume the rows start at 0 */
+	dims[X] = rho->real_shape[X];
+	dims[Y] = rho->shape[Y];
 
-	//dataspace_id = H5Screate_simple(2, dims, NULL);
-	//dataset_id = H5Dcreate1(file_id, "/rho", H5T_NATIVE_DOUBLE, dataspace_id, H5P_DEFAULT);
-	//status = H5Dwrite(dataset_id, H5T_NATIVE_DOUBLE, H5S_ALL, H5S_ALL, H5P_DEFAULT, rho->data);
-	//status = H5Dclose(dataset_id);
+	dataspace_id = H5Screate_simple(2, dims, NULL);
+	dataset_id = H5Dcreate1(file_id, "/rho", H5T_NATIVE_DOUBLE, dataspace_id, H5P_DEFAULT);
+	status = H5Dwrite(dataset_id, H5T_NATIVE_DOUBLE, H5S_ALL, H5S_ALL, H5P_DEFAULT, rho->data);
+	status = H5Dclose(dataset_id);
 
-	phi = sim->field._phi;
+	phi = sim->field.phi;
+	_phi = sim->field._phi;
 
-	ix = phi->shape[X] - 2;
+	ix = _phi->shape[X] - 2;
 	/* Erase the FFT padding */
-	for(iy=0; iy<phi->shape[Y]; iy++)
+	for(iy=2; iy<phi->shape[Y]-1; iy++)
 	{
-		MAT_XY(phi, ix, iy) = 0.0;
-		MAT_XY(phi, ix+1, iy) = 0.0;
+		MAT_XY(_phi, ix, iy) = NAN;
+		MAT_XY(_phi, ix+1, iy) = NAN;
 	}
 
-	dims[X] = phi->shape[X];
+	dims[X] = phi->real_shape[X];
 	dims[Y] = phi->shape[Y];
 	dataspace_id = H5Screate_simple(2, dims, NULL);
 	dataset_id = H5Dcreate1(file_id, "/phi", H5T_NATIVE_DOUBLE, dataspace_id, H5P_DEFAULT);
