b23fcecedbbab72fd1e7377cccfa77b90110bed4
Rodrigo <rodarima@gmail.com>
2020-04-27 17:29:36 +0200

    Add position_delta init method

--- src/particle.c
+++ src/particle.c
@@ -14,94 +14,13 @@
 #include <utlist.h>
 #include <string.h>
 
-//int
-//init_default(sim_t *sim, plasma_chunk_t *chunk, particle_set_t *set);
-
-int
-init_randpos(sim_t *sim, pchunk_t *chunk, pset_t *set);
-
-//int
-//init_position_delta(sim_t *sim, plasma_chunk_t *chunk, particle_set_t *set);
-
-particle_config_t pc[] =
-{
-	{"random position",		init_randpos},
-//	{"position delta",		init_position_delta},
-//	{"default",			init_default},
-	{NULL, NULL}
-};
-
-//particle_t *
-//particle_init()
-//{
-//	particle_t *p;
-//
-//	p = safe_malloc(sizeof(*p));
-//
-//	/* As we send the particle via MPI_Send directly, some wholes don't get
-//	 * initialized, thus we use memset meanwhile */
-//
-//	/* TODO: Use a packed version of particle_t for MPI */
-//	memset(p, 0, sizeof(*p));
-//
-//	p->next = NULL;
-//	p->prev = NULL;
-//
-//	return p;
-//}
-
-int
-particles_init(sim_t *sim, pchunk_t *chunk, pset_t *set)
-{
-	int i;
-	const char *method;
-	config_setting_t *cs;
-	specie_t *s;
-
-	s = set->info;
-	cs = s->conf;
-
-	if(config_setting_lookup_string(cs, "init_method", &method) != CONFIG_TRUE)
-	{
-		err("WARNING: Particle init method for specie \"%s\" not specified. Using \"default\".\n",
-				s->name);
-		method = "default";
-	}
-
-	for(i = 0; pc[i].name; i++)
-	{
-		if(strcmp(pc[i].name, method) != 0)
-			continue;
-
-		if(!pc[i].init)
-		{
-			err("The init method is NULL, aborting.\n");
-			exit(1);
-		}
-
-		return pc[i].init(sim, chunk, set);
-	}
-
-	err("Unknown init method \"%s\", aborting.\n", method);
-	exit(1);
-
-	return 0;
-}
-
-//static int
-//init_default(sim_t *sim, pchunk_t *chunk, pset_t *set)
-//{
-//	return init_randpos(sim, chunk, set);
-//}
-
 static double
 uniform(double a, double b)
 {
 	return rand() / (RAND_MAX + 1.0) * (b - a) + a;
 }
 
-
-int
+static int
 init_randpos(sim_t *sim, pchunk_t *chunk, pset_t *set)
 {
 	plist_t *l;
@@ -168,51 +87,130 @@ init_randpos(sim_t *sim, pchunk_t *chunk, pset_t *set)
 	return 0;
 }
 
-//int
-//init_position_delta(sim_t *sim, plasma_chunk_t *chunk, particle_set_t *set)
-//{
-//	int d;
-//	particle_t *p;
-//	double r[MAX_DIM] = {0};
-//	double dr[MAX_DIM] = {0};
-//	double *L;
-//	double v[MAX_DIM] = {0};
-//	config_setting_t *cs, *cs_v, *cs_r, *cs_dr;
-//
-//	cs = set->info->conf;
-//	L = sim->L;
-//
-//	cs_v = config_setting_get_member(cs, "drift_velocity");
-//	if(config_array_float(cs_v, v, sim->dim))
-//		return 1;
-//
-//	cs_dr = config_setting_get_member(cs, "position_delta");
-//	if(config_array_float(cs_dr, dr, sim->dim))
-//		return 1;
-//
-//	cs_r = config_setting_get_member(cs, "position_init");
-//	if(config_array_float(cs_r, r, sim->dim))
-//		return 1;
-//
-//	dbg("Init position and speed in %d particles\n", set->nparticles);
-//
-//	for(p = set->particles; p; p = p->next)
-//	{
-//		for(d=0; d<sim->dim; d++)
-//		{
-//			p->u[d] = v[d];
-//
-//			WRAP(p->x[d], r[d] + dr[d] * p->i, L[d]);
-//			p->E[d] = 0.0;
-//		}
-//
-//		if(p->i < 100)
-//			dbg("Particle %d offset init at (%e, %e) in chunk (%d, %d)\n",
-//				p->i, p->x[X], p->x[Y], chunk->ig[X], chunk->ig[Y]);
-//	}
-//
-//	return 0;
-//}
+static int
+init_position_delta(sim_t *sim, pchunk_t *chunk, pset_t *set)
+{
+	f64 r[MAX_DIM] = {0};
+	f64 dr[MAX_DIM] = {0};
+	f64 *L;
+	f64 v[MAX_DIM] = {0};
+	config_setting_t *cs, *cs_v, *cs_r, *cs_dr;
+	plist_t *l;
+	pblock_t *b;
+	ppack_t *p;
+	i64 d, i, iv;
+
+	UNUSED(chunk);
+
+	l = &set->list;
+	assert(chunk);
+	cs = set->info->conf;
+	L = sim->L;
+
+	cs_v = config_setting_get_member(cs, "drift_velocity");
+	if(config_array_float(cs_v, v, sim->dim))
+		return 1;
+
+	cs_dr = config_setting_get_member(cs, "position_delta");
+	if(config_array_float(cs_dr, dr, sim->dim))
+		return 1;
+
+	cs_r = config_setting_get_member(cs, "position_init");
+	if(config_array_float(cs_r, r, sim->dim))
+		return 1;
+
+	for(b = l->b; b; b = b->next)
+	{
+		for(i=0; i < l->max_packs; i++)
+		{
+			//dbg("Initialization of ppack %ld\n", i);
+			p = &b->p[i];
+
+			for(iv=0; iv<MAX_VEC; iv++)
+			{
+				/* Initialize only up to b->n */
+				if(i*MAX_VEC + iv >= b->n)
+				{
+#ifdef USE_PPACK_MAGIC
+					/* Set the magic to help
+					 * valgrind */
+					p->magic[iv] = MAGIC_UNDEF;
+#endif
+					continue;
+				}
+#ifdef USE_PPACK_MAGIC
+				p->magic[iv] = MAGIC_PARTICLE;
+#endif
+				/* XXX: Should we keep the particles out of
+				 * b->n inside the chunk? */
+				for(d=0; d<Z; d++)
+					WRAP(p->r[d][iv], r[d] + dr[d] * p->i[iv], L[d]);
+
+				p->r[Z][iv] = 0.0;
+
+				/* FIXME: Separate position and velocity init metods */
+				p->u[X][iv] = v[X];
+				p->u[Y][iv] = v[Y];
+				p->u[Z][iv] = 0.0;
+			}
+
+			p->E[X] = vf64_set1(0.0);
+			p->E[Y] = vf64_set1(0.0);
+			p->E[Z] = vf64_set1(0.0);
+
+			p->B[X] = vf64_set1(sim->B[X]);
+			p->B[Y] = vf64_set1(sim->B[Y]);
+			p->B[Z] = vf64_set1(sim->B[Z]);
+		}
+	}
+
+	return 0;
+}
+
+static particle_config_t pc[] =
+{
+	{"random position",		init_randpos},
+	{"position delta",		init_position_delta},
+	{NULL, NULL}
+};
+
+int
+particles_init(sim_t *sim, pchunk_t *chunk, pset_t *set)
+{
+	int i;
+	const char *method;
+	config_setting_t *cs;
+	specie_t *s;
+
+	s = set->info;
+	cs = s->conf;
+
+	if(config_setting_lookup_string(cs, "init_method", &method) != CONFIG_TRUE)
+	{
+		err("Particle init method for specie \"%s\" not specified.\n",
+				s->name);
+		exit(1);
+	}
+
+	for(i = 0; pc[i].name; i++)
+	{
+		if(strcmp(pc[i].name, method) != 0)
+			continue;
+
+		if(!pc[i].init)
+		{
+			err("The init method is NULL, aborting.\n");
+			exit(1);
+		}
+
+		return pc[i].init(sim, chunk, set);
+	}
+
+	err("Unknown init method \"%s\", aborting.\n", method);
+	exit(1);
+
+	return 0;
+}
 
 static int
 chunk_E(sim_t *sim, int i)
